6430c7b94211436825d36a7b5a6c73b38ba55ed2
Mat Benfield <1188107+MatBenfield@users.noreply.github.com>
2020-08-26 12:10:12 +0100

    Create get_blog_posts.py

---
+++ .github/workflows/get_blog_posts.py
@@ -0,0 +1,30 @@
+import feedparser
+
+def replace_chunk(content, marker, chunk):
+    r = re.compile(
+        r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
+        re.DOTALL,
+    )
+    chunk = "<!-- {} starts -->\n{}\n<!-- {} ends -->".format(marker, chunk, marker)
+    return r.sub(chunk, content)
+
+def fetch_blog_entries():
+    entries = feedparser.parse("https://thechels.uk/feed/")["entries"]
+    return [
+        {
+            "title": entry["title"],
+            "url": entry["link"].split("#")[0],
+            "published": entry["published"].split("T")[0],
+        }
+        for entry in entries
+    ]
+
+if __name__ == "__main__":
+    readme = root / "README.md"
+    entries = fetch_blog_entries()[:5]
+    entries_md = "\n".join(
+        ["* [{title}]({url}) - {published}".format(**entry) for entry in entries]
+    )
+    rewritten = replace_chunk(rewritten, "blog", entries_md)
+
+    readme.open("w").write(rewritten)
