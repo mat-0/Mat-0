ef1b41360521471b20c1dbeee9fe380cfe0b34ef
MatBenfield <MatBenfield@users.noreply.github.com>
2020-09-01 21:32:45 +0100

    linting
    Signed-off-by: MatBenfield <MatBenfield@users.noreply.github.com>

--- python/get_blog_posts.py
+++ python/get_blog_posts.py
@@ -2,12 +2,14 @@
 Get RSS feed
 """
 import re
-import os
-import feedparser
 import pathlib
+import feedparser
 
 root = pathlib.Path(__file__).parent.parent.resolve()
 
+"""
+replacer
+"""
 def replace_chunk(content, marker, chunk):
     replacer = re.compile(
         r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
@@ -16,6 +18,9 @@ def replace_chunk(content, marker, chunk):
     chunk = "<!-- {} starts -->\n{}\n<!-- {} ends -->".format(marker, chunk, marker)
     return replacer.sub(chunk, content)
 
+"""
+Get blog posts
+"""
 def fetch_blog_entries():
     entries = feedparser.parse("https://thechels.uk/feed.xml")["entries"]
     return [
@@ -30,11 +35,10 @@ def fetch_blog_entries():
 if __name__ == "__main__":
     readme = root / "README.md"
     readme_contents = readme.open().read()
-    
     entries = fetch_blog_entries()[:5]
     entries_md = "\n".join(
         ["- [{title}]({url}) - {published}".format(**entry) for entry in entries]
     )
-    
+
     rewritten = replace_chunk(readme_contents, "blog", entries_md)
     readme.open("w").write(rewritten)
