3f0a37cda9287c1928c8448518babe4f61043ee8
MatBenfield <MatBenfield@users.noreply.github.com>
2020-09-21 20:22:37 +0100

    toggle light with 45 sleep
    Signed-off-by: MatBenfield <MatBenfield@users.noreply.github.com>

--- python/update_lights.py
+++ python/update_lights.py
@@ -1,10 +1,12 @@
 import urllib.request
 import urllib.parse
 import os
-
+import time
 # personal details
 url = os.getenv('IFTTT_URL')
-
 # Processing
-f = urllib.request.urlopen(url)
-print(f.read().decode('utf-8'))
+a = urllib.request.urlopen(url)
+print(a.read().decode('utf-8'))
+time.sleep(45)
+b = urllib.request.urlopen(url)
+print(b.read().decode('utf-8'))
